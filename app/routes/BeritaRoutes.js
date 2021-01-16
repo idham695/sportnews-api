f59335464b5a0e7a93614d8de472629807e113fe
Idham Kholid <idhamkholid965@gmail.com>
2021-01-16 19:19:29 +0700

    update

--- app/routes/BeritaRoutes.js
+++ app/routes/BeritaRoutes.js
@@ -5,7 +5,7 @@ import authJwt from "../middleware/authJwt";
 const router = Router();
 
 router.post("/add_berita", berita.create);
-router.get("/get_all_berita", berita.findAll);
+router.get("/get_all_berita", authJwt.verifyToken, berita.findAll);
 router.get("/get_berita_by_id/:id", berita.findOne);
 router.put("/update_berita/:id", berita.update);
 router.delete("/delete_berita/:id", berita.delete);
