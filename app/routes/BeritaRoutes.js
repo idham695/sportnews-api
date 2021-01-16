46938b179d65b38b6836b3a2625dd3e20f4466f4
Idham Kholid <idhamkholid965@gmail.com>
2021-01-16 19:24:33 +0700

    update

--- app/routes/BeritaRoutes.js
+++ app/routes/BeritaRoutes.js
@@ -4,10 +4,10 @@ import authJwt from "../middleware/authJwt";
 
 const router = Router();
 
-router.post("/add_berita", berita.create);
+router.post("/add_berita", authJwt.verifyToken, berita.create);
 router.get("/get_all_berita", authJwt.verifyToken, berita.findAll);
-router.get("/get_berita_by_id/:id", berita.findOne);
-router.put("/update_berita/:id", berita.update);
-router.delete("/delete_berita/:id", berita.delete);
+router.get("/get_berita_by_id/:id", authJwt.verifyToken, berita.findOne);
+router.put("/update_berita/:id", authJwt.verifyToken, berita.update);
+router.delete("/delete_berita/:id", authJwt.verifyToken, berita.delete);
 
 export default router;
